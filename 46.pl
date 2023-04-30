929d899a67c75bcce0d6b25f13888be873778ad3
MahmoudJobeel <mahmoudjobeel@gmail.com>
2023-04-30 19:20:12 +0300

    Adding boolean expression
    Adding boolean expressions like x==y. And testing it as shown in tests.pl file

--- 46.pl
+++ 46.pl
@@ -3,10 +3,17 @@ s(Tree) --> assignment(Tree), eos.
 
 assignment(assign(Id, Expr,End)) --> identifier(Id), ['='], expr(Expr),semi_colon(End).
 
+identifier(id(X)) -->
+    [First], { catch(code_type(First, alpha), _, fail) ; First = '_'},
+    identifier_tail(Tail),
+    { atom_chars(X, [First|Tail]) }.
 
-identifier(id(X)) --> [First], { code_type(First, alpha) ; First = '_'}, identifier_tail(Tail), { atom_chars(X, [First|Tail]) }.
-identifier_tail([Char|Tail]) --> [Char], { code_type(Char, alnum) ; Char = '_' }, identifier_tail(Tail).
+identifier_tail([Char|Tail]) -->
+    [Char], { catch(code_type(Char, alnum), _, fail) ; Char = '_' },
+    identifier_tail(Tail).
 identifier_tail([]) --> [].
+
+boolean(boolean(Left,Op,Right)) --> expr(Left), operator_Compare(Op),expr(Right).
 
 expr(expr(Term)) --> term(Term).
 expr(expr(Left, Op, Right)) --> term(Left), operator(Op), expr(Right).
@@ -24,22 +31,15 @@ operator(mul) --> ['*'].
 operator(div) --> ['/'].
 operator(mod) --> ['%'].
 
+operator_Compare(equal) --> ['=='].
+operator_Compare(inequality) --> ['!='].
+operator_Compare(less_equal) --> ['<='].
+operator_Compare(less) --> ['<'].
+operator_Compare(greater_equal) --> ['>='].
+operator_Compare(greater) --> ['>'].
+
 semi_colon(end) -->[';'].
 
 num(num(X)) --> [X], {number(X)}.
 
 eos --> [].
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tests.pl
+++ tests.pl
@@ -6,3 +6,10 @@ s(Tree,['_',r, u, n, =, 2, *, r, u, n, +, 1, ;],[]). %True
 
 s(Tree,['1' ,=, 2, *, r, u, n, +, 1, ;],[]). %false 
 
+boolean(Tree,['(' , 'C', o, m, p, i, l, e, r, >, o, t, h, e, r, s, ')'],[]). %False
+
+boolean(Tree,['C', o, m, p, i, l, e, r, >, o, t, h, e, r, s],[]). %True
+
+boolean(Tree,[r, u, n, <=, w, -, 1],[]). %True
+
+boolean(Tree,[r, u, n, <=, '_',w, -, 1],[]). %True
