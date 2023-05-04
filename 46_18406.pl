bcc9d1b40b06d53cfc181a04c9def4ad8265a8dd
MahmoudJobeel <mahmoudjobeel@gmail.com>
2023-05-05 01:02:10 +0300

    removing epsilon

--- 46_18406.pl
+++ 46_18406.pl
@@ -1,5 +1,5 @@
 % Define grammar rules
-s(Tree) --> (assignment(Tree); conditional(Tree)), eos.
+s(Tree) --> (assignment(Tree); conditional(Tree)). 
 
 conditional(if_else(If, Else)) --> if_statement(If), ['else'], s(Else).
 conditional(if(If)) --> if_statement(If).
@@ -14,7 +14,7 @@ assignment(assign(Id, Expr,End,Body)) --> identifier(Id), ['='], expr(Expr),semi
 
 identifier_tail([Char|Tail]) -->
     [Char], { 
-        ( catch(number(Char), _, fail), Char >= 0, Char =< 9 )  % digit
+        ( catch(number(Char), _, fail), Char >= 0, Char =< 9 ) 
     },
     identifier_tail(Tail).
 
@@ -59,64 +59,3 @@ operator_Compare(greater) --> ['>'].
 semi_colon(end) -->[';'].
 
 num(num(X)) --> [X], {number(X)}.
-
-eos --> [].
-
-% % Define grammar rules
-% s(Tree) --> (assignment(Tree); conditional(Tree)), eos.
-
-% conditional(if_else(If, Else)) --> if_statement(If), ['else'], s(Else).
-% conditional(if(If)) --> if_statement(If).
-% conditional(while(While,Body)) --> ['while'], ['('], boolean(While), [')'], s(Body).
-
-% if_statement((Expr, Body)) --> ['if'], ['('], boolean(Expr), [')'], s(Body).
-
-% boolean(boolean(Left,Op,Right)) --> expr(Left), operator_Compare(Op),expr(Right).
-
-% assignment(assign(Id, Expr,End)) --> identifier(Id), ['='], expr(Expr),semi_colon(End).
-% assignment(assign(Id, Expr,End,Body)) --> identifier(Id), ['='], expr(Expr),semi_colon(End),s(Body).
-
-% identifier_tail([Char|Tail]) -->
-%     [Char], { 
-%         ( catch(number(Char), _, fail), Char >= 0, Char =< 9 ) ; % digit
-%         code_type(Char, alnum) ; % alphanumeric
-%         Char = '_'; 
-%         Char = '$' 
-%     },
-%     identifier_tail(Tail).
-
-% identifier_tail([]) --> [].
-
-% identifier(id(X)) -->
-%     [First], { catch(code_type(First, alpha), _, fail) ; First = '_' ; First='$'},
-%     identifier_tail(Tail),
-%     { append([First|Tail], [], X) }.
-
-% expr(expr(Term)) --> term(Term).
-% expr(expr(Left, Op, Right)) --> term(Left), operator(Op), expr(Right).
-
-% term(term(Factor)) --> factor(Factor).
-% term(term(Left, Op, Right)) --> factor(Left), operator(Op), term(Right).
-
-% factor(factor(Num)) --> num(Num).
-% factor(factor(Id)) --> identifier(Id).
-% factor(factor(Expr)) --> ['('], expr(Expr), [')'].
-
-% operator(add) --> ['+'].
-% operator(sub) --> ['-'].
-% operator(mul) --> ['*'].
-% operator(div) --> ['/'].
-% operator(mod) --> ['%'].
-
-% operator_Compare(equal) --> ['=='].
-% operator_Compare(inequality) --> ['!='].
-% operator_Compare(less_equal) --> ['<='].
-% operator_Compare(less) --> ['<'].
-% operator_Compare(greater_equal) --> ['>='].
-% operator_Compare(greater) --> ['>'].
-
-% semi_colon(end) -->[';'].
-
-% num(num(X)) --> [X], {number(X)}.
-
-% eos --> [].
--- tests.pl
+++ tests.pl
@@ -35,68 +35,34 @@ s(T, ['$','$','a', =, 1,+,2, ;] ,[]). %True
 % TESTS
 
 % SHOULD ACCEPT
-% ?- s(T, [if,'(',a,<,z,')',b,=,w,;,else,m,=,1,;] ,[]).
-
+s(T, [if,'(',a,<,z,')',b,=,w,;,else,m,=,1,;] ,[]).
 % if(a<z)b=w;elsem=1;
-% T = [ ifelse( cmp_exp(var([a]), cop(<), var([z])),
-% 		assign(var([b]), value(var([w]))),
-% 		assign(var([m]), value(int(1)))) ] .
 
-% ?- s(T, [while,'(',1,>,2,')',z,=,3,;,a,=,b,;] ,[]).
 
+s(T, [while,'(',1,>,2,')',z,=,3,;,a,=,b,;] ,[]).
 % while(1>2)z=3;a=b;
-% T = [
-% while( cmp_exp(int(1), cop(>), int(2)), 
-% 		assign(var([z]), value(int(3)))
-% ),
-% assign(var([a]), value(var([b])))] .
 
-% ?- s(T, [while,'(',1,>,2,')',z,=,3,;,a,=,b,;,if,'(',a,<,z,')',b,=,w,;,else,m,=,1,;] ,[]).
 
+s(T, [while,'(',1,>,2,')',z,=,3,;,a,=,b,;,if,'(',a,<,z,')',b,=,w,;,else,m,=,1,;] ,[]).
 % while(1>2)z=3;a=b;if(a<z)b=w;else m=1;
-% T = [
-% while( cmp_exp(int(1), cop(>), int(2)),
-% 			assign(var([z]), value(int(3)))
-% ), 
-% assign(var([a]), value(var([b]))),
-% ifelse( cmp_exp(var([a]), cop(<), var([z])),
-% 		assign(var([b]), value(var([w]))),
-% 		assign(var([m]), value(int(1)))
-% )] .
 
-% ?- s(T, [while,'(',1,>,2,')',z,=,3,;,a,=,b,;,if,'(',a,<,z,')',b,=,w,;] ,[]).
 
+s(T, [while,'(',1,>,2,')',z,=,3,;,a,=,b,;,if,'(',a,<,z,')',b,=,w,;] ,[]).
 % while(1>2)z=3;a=b;if(a<z)b=w;
-% T = [
-% while( cmp_exp(int(1), cop(>), int(2)),
-% 	assign(var([z]), value(int(3)))
-% ),
-% assign(var([a]), value(var([b]))),
-% if( cmp_exp(var([a]), cop(<), var([z])),
-% 	assign(var([b]), value(var([w])))
-% )] .
 
-% ?- s(T, ['_','a','_', =, 1,+,2, ;] ,[]).
 
-% T = [assign(var(['_', a, '_']), value(expr(int(1), aop(+), int(2))))] .
+s(T, ['_','a','_', =, 1,+,2, ;] ,[]).
 
-% ?- s(T, ['$','$','a', =, 1,+,2, ;] ,[]).
+s(T, ['$','$','a', =, 1,+,2, ;] ,[]).
 
-% T = [assign(var([$, $, a]), value(expr(int(1), aop(+), int(2))))] .
+s(T, ['a','b','$','_', =, 1,+,2, ;] ,[]).
 
-% ?- s(T, ['a','b','$','_', =, 1,+,2, ;] ,[]).
 
-% T = [assign(var([a, b, $, '_']), value(expr(int(1), aop(+), int(2))))] .
-
-% ?- s(T, [c,o,u,n,t,e,r,1,=,c,o,u,n,t,e,r,1,+,'(',x,/,y,-,21,')','%','_',w,2,;], []).
-
+s(T, [c,o,u,n,t,e,r,1,=,c,o,u,n,t,e,r,1,+,'(',x,/,y,-,21,')','%','_',w,2,;], []).
 % counter1 = counter1 + (x / y - 21) % _w2;
-% T = [
-% assign(var([c, o, u, n, t, e, r, 1]),
-%  value(expr(var([c, o, u, n, t, e, r, 1]), aop(+), expr(expr(expr(var([x]), aop(/), var([y])), aop(-), int(21)), aop("%"), var(['_', w, 2]))))
-% )] .
 
-% ?- s(T, [c,o,u,n,t,e,r,=,x,+,y,;,while,'(',c,o,u,n,t,e,r,<=,w,-,1,')',while,'(',c,o,u,n,t,e,r,'!=',y,')',c,o,u,n,t,e,r,=,c,o,u,n,t,e,r,+,x,+,5,;,if,'(',c,o,u,n,t,e,r,>,w,+,2,')',if,'(',c,o,u,n,t,e,r,>,x,')',c,o,u,n,t,e,r,=,y,;,else,if,'(',c,o,u,n,t,e,r,>,y,')',c,o,u,n,t,e,r,=,x,;,w,=,y,/,x,;] ,[]).
+
+s(T, [c,o,u,n,t,e,r,=,x,+,y,;,while,'(',c,o,u,n,t,e,r,<=,w,-,1,')',while,'(',c,o,u,n,t,e,r,'!=',y,')',c,o,u,n,t,e,r,=,c,o,u,n,t,e,r,+,x,+,5,;,if,'(',c,o,u,n,t,e,r,>,w,+,2,')',if,'(',c,o,u,n,t,e,r,>,x,')',c,o,u,n,t,e,r,=,y,;,else,if,'(',c,o,u,n,t,e,r,>,y,')',c,o,u,n,t,e,r,=,x,;,w,=,y,/,x,;] ,[]).
 
 % counter = x + y;
 % while (counter <= w - 1)
@@ -109,88 +75,47 @@ s(T, ['$','$','a', =, 1,+,2, ;] ,[]). %True
 % 		if (counter > y)
 % 			counter = x;
 % w = y / x;
-% T = [
-% assign(var([c, o, u, n, t, e, r]),
-% 	value( expr(var([x]), aop(+), var([y])))
-% ),
-% while( cmp_exp(var([c, o, u, n, t, e, r]), cop(<=), expr(var([w]), aop(-), int(1))),
-% 	while( cmp_exp(var([c, o, u, n, t, e, r]), cop("!="), var([y])),
-% 		assign(var([c, o, u, n, t, e, r]), value(expr(var([c, o, u, n, t, e, r]), aop(+), expr(var([x]), aop(+), int(5)))))
-% 	)
-% ),
-% if( cmp_exp(var([c, o, u, n, t, e, r]), cop(>), expr(var([w]), aop(+), int(2))),
-% 	ifelse( cmp_exp(var([c, o, u, n, t, e, r]), cop(>), var([x])),
-% 		assign(var([c, o, u, n, t, e, r]), value(var([y]))),
-% 		if( cmp_exp(var([c, o, u, n, t, e, r]), cop(>), var([y])),
-% 			assign(var([c, o, u, n, t, e, r]), value(var([x])))))
-% ),
-% assign(var([w]), value(expr(var([y]), aop(/), var([x]))))].
 
 
-% ?- s(T, [r,u,n,=,0,;,while,'(',r,u,n,<=,w,-,1,')',r,u,n,=,2,*,r,u,n,+,1,;] ,[]).
 
+s(T, [r,u,n,=,0,;,while,'(',r,u,n,<=,w,-,1,')',r,u,n,=,2,*,r,u,n,+,1,;] ,[]).
 % % run=0;while(run<=w-1)run=2*run+1;
-% T = [
-% assign(var([r, u, n]), value(int(0))),
-% while( cmp_exp(var([r, u, n]), cop(<=), expr(var([w]), aop(-), int(1))),
-% 	assign(var([r, u, n]), value( expr(expr(int(2), aop(*), var([r, u, n])), aop(+),int(1))))
-% )] .
+
+s(T, [c,o,u,n,t,e,r,1,=,c,o,u,n,t,e,r,1,+,'(',x,/,y,-,21,')','%','_',w,2,;], []).
 
 
-% ?- s(T, [if,'(','C',o,m,p,i,l,e,r,>,o,t,h,e,r,s,')',a,l,l,=,1,;] ,[]).
-
+s(T, [if,'(','C',o,m,p,i,l,e,r,>,o,t,h,e,r,s,')',a,l,l,=,1,;] ,[]).
 % % if(Compiler>others)all=1;
-% T = [
-% if( cmp_exp(var(['C', o, m, p, i, l, e, r]), cop(>), var([o, t, h, e, r, s])),
-% 	assign(var([a, l, l]), value(int(1)))
-% )] .
 
-% ?- s(T, [while,'(',r,u,n,<=,w,-,1,')',r,u,n,=,2,*,r,u,n,+,1,;] ,[]).
-
+s(T, [while,'(',r,u,n,<=,w,-,1,')',r,u,n,=,2,*,r,u,n,+,1,;] ,[]).
 % % while(run<=w-1)run=2*run+1;
-% T = [
-% while( cmp_exp(var([r, u, n]), cop(<=), expr(var([w]), aop(-), int(1))),
-% 		assign(var([r, u, n]), value(expr(expr(int(2), aop(*), var([r, u, n])), aop(+), int(1))))
-% )] .
 
-% ?- s(T, [if,'(','T',o,t,a,l,<,500,')','T',o,t,a,l,=,'T',o,t,a,l,+,150,;,else,if,'(',x,'!=','T',o,t,a,l,')','T',o,t,a,l,=,0,;,else,x,=,55,/,y,-,15,;] ,[]).
+
+s(T, [if,'(','T',o,t,a,l,<,500,')','T',o,t,a,l,=,'T',o,t,a,l,+,150,;,else,if,'(',x,'!=','T',o,t,a,l,')','T',o,t,a,l,=,0,;,else,x,=,55,/,y,-,15,;] ,[]).
 
 % % if(Total<500)Total=Total+150;elseif(x!=Total)Total=0;elsex=55/y-15;
-% T = [
-% ifelse( cmp_exp(var(['T', o, t, a, l]), cop(<), int(500)),
-% 	assign(var(['T', o, t, a, l]), value(expr(var(['T', o, t, a, l]), aop(+), int(150)))),
-% 	ifelse( cmp_exp(var([x]), cop("!="), var(['T', o, t, a, l])),
-% 		assign(var(['T', o, t, a, l]), value(int(0))),
-% 		assign(var([x]), value(expr(expr(int(55), aop(/), var([y])), aop(-), int(15))))
-% 	)
-% )] .
-
 
 
 % % NOT ACCEPTED
 
-% ?- s(T, [v,=,23,*,s,a,l,+,1,a,z,;], []).
-
+s(T, [v,=,23,*,s,a,l,+,1,a,z,;], []).
 % % v=23*sal+1az;
 
-% ?- s(T, [x,=,z,;,if,'(',x,>,y,')',x,'_',n,e,w,=,z,;,else,x,'_',n,e,w,=,y,;,else,y,=,z,;] ,[]).
-
+s(T, [x,=,z,;,if,'(',x,>,y,')',x,'_',n,e,w,=,z,;,else,x,'_',n,e,w,=,y,;,else,y,=,z,;] ,[]).
 % % x=z;if(x>y)x_new=z;elsex_new=y;elsey=z;
 
-% ?- s(T, [if,'(',x,>,0,')',x,=,x,-,1], []).
-
+s(T, [if,'(',x,>,0,')',x,=,x,-,1], []).
 % % if(x>0)x=x-1
 
-% ?- s(T, [c,o,u,n,t,e,r,1,=,1,c,o,u,n,t,e,r,+,+,x,/,y,-,21,')','%','_',w,2] ,[]).
-
+s(T, [c,o,u,n,t,e,r,1,=,1,c,o,u,n,t,e,r,+,+,x,/,y,-,21,')','%','_',w,2] ,[]).
 % counter1 = 1counter ++x / y - 21) % _w2
 
-% ?- s(T, [a, =, if, ;] ,[]).
+s(T, [a, =, if, ;] ,[]).
 
-% ?- s(T, ['a','b',';','_', =, 1,+,2, ;] ,[]).
+s(T, ['a','b',';','_', =, 1,+,2, ;] ,[]).
 
-% ?- s(T, ['a','b','!','_', =, 1,+,2, ;] ,[]).
+s(T, ['a','b','!','_', =, 1,+,2, ;] ,[]).
 
-% ?- s(T, [w,h,i,l,e,'(',1,>,2,')',z,=,3,;,a,=,b,;] ,[]).
+s(T, [w,h,i,l,e,'(',1,>,2,')',z,=,3,;,a,=,b,;] ,[]).
 
 
