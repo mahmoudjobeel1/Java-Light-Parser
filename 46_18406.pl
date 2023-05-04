e0595d50dcc0f8259760ccf11320edeaf07a8549
MahmoudJobeel <mahmoudjobeel@gmail.com>
2023-05-05 00:46:30 +0300

    Fixing identifier

--- 46_18406.pl
+++ 46_18406.pl
@@ -12,15 +12,26 @@ boolean(boolean(Left,Op,Right)) --> expr(Left), operator_Compare(Op),expr(Right)
 assignment(assign(Id, Expr,End)) --> identifier(Id), ['='], expr(Expr),semi_colon(End).
 assignment(assign(Id, Expr,End,Body)) --> identifier(Id), ['='], expr(Expr),semi_colon(End),s(Body).
 
-identifier(id(X)) -->
-    [First], { catch(code_type(First, alpha), _, fail) ; First = '_'},
-    identifier_tail(Tail),
-    { atom_chars(X, [First|Tail]) }.
+identifier_tail([Char|Tail]) -->
+    [Char], { 
+        ( catch(number(Char), _, fail), Char >= 0, Char =< 9 )  % digit
+    },
+    identifier_tail(Tail).
 
 identifier_tail([Char|Tail]) -->
-    [Char], { catch(code_type(Char, alnum), _, fail) ; Char = '_' },
+    [Char], { 
+        ( catch(code_type(Char, alnum) , _, fail)); 
+        Char = '_'; 
+        Char = '$' 
+    },
     identifier_tail(Tail).
+
 identifier_tail([]) --> [].
+
+identifier(id(X)) -->
+    [First], { catch(code_type(First, alpha), _, fail) ; First = '_' ; First='$'},
+    identifier_tail(Tail),
+    { append([First|Tail], [], X) }.
 
 expr(expr(Term)) --> term(Term).
 expr(expr(Left, Op, Right)) --> term(Left), operator(Op), expr(Right).
@@ -50,3 +61,62 @@ semi_colon(end) -->[';'].
 num(num(X)) --> [X], {number(X)}.
 
 eos --> [].
+
+% % Define grammar rules
+% s(Tree) --> (assignment(Tree); conditional(Tree)), eos.
+
+% conditional(if_else(If, Else)) --> if_statement(If), ['else'], s(Else).
+% conditional(if(If)) --> if_statement(If).
+% conditional(while(While,Body)) --> ['while'], ['('], boolean(While), [')'], s(Body).
+
+% if_statement((Expr, Body)) --> ['if'], ['('], boolean(Expr), [')'], s(Body).
+
+% boolean(boolean(Left,Op,Right)) --> expr(Left), operator_Compare(Op),expr(Right).
+
+% assignment(assign(Id, Expr,End)) --> identifier(Id), ['='], expr(Expr),semi_colon(End).
+% assignment(assign(Id, Expr,End,Body)) --> identifier(Id), ['='], expr(Expr),semi_colon(End),s(Body).
+
+% identifier_tail([Char|Tail]) -->
+%     [Char], { 
+%         ( catch(number(Char), _, fail), Char >= 0, Char =< 9 ) ; % digit
+%         code_type(Char, alnum) ; % alphanumeric
+%         Char = '_'; 
+%         Char = '$' 
+%     },
+%     identifier_tail(Tail).
+
+% identifier_tail([]) --> [].
+
+% identifier(id(X)) -->
+%     [First], { catch(code_type(First, alpha), _, fail) ; First = '_' ; First='$'},
+%     identifier_tail(Tail),
+%     { append([First|Tail], [], X) }.
+
+% expr(expr(Term)) --> term(Term).
+% expr(expr(Left, Op, Right)) --> term(Left), operator(Op), expr(Right).
+
+% term(term(Factor)) --> factor(Factor).
+% term(term(Left, Op, Right)) --> factor(Left), operator(Op), term(Right).
+
+% factor(factor(Num)) --> num(Num).
+% factor(factor(Id)) --> identifier(Id).
+% factor(factor(Expr)) --> ['('], expr(Expr), [')'].
+
+% operator(add) --> ['+'].
+% operator(sub) --> ['-'].
+% operator(mul) --> ['*'].
+% operator(div) --> ['/'].
+% operator(mod) --> ['%'].
+
+% operator_Compare(equal) --> ['=='].
+% operator_Compare(inequality) --> ['!='].
+% operator_Compare(less_equal) --> ['<='].
+% operator_Compare(less) --> ['<'].
+% operator_Compare(greater_equal) --> ['>='].
+% operator_Compare(greater) --> ['>'].
+
+% semi_colon(end) -->[';'].
+
+% num(num(X)) --> [X], {number(X)}.
+
+% eos --> [].
